01330e04065229197425f03d1bf66c17cc240fd2
Luci Furtun <lucianfurtun@gmail.com>
2017-02-19 22:07:31 +0200

    Add api settings

---
+++ pencepay/settings/choices.py
@@ -0,0 +1,19 @@
+class ActionChoices:
+    CREATE = 'create'
+    FIND = 'find'
+    SEARCH = 'search'
+    VOID = 'void'
+    CAPTURE = 'capture'
+    REFUND = 'refund'
+    UPDATE = 'update'
+    DELETE = 'delete'
+
+
+class APIChoices:
+    TRANSACTIONS = 'transactions'
+    ADDRESSES = 'addresses'
+    CUSTOMERS = 'customers'
+    CARDS = 'cards'
+    BANK_ACCOUNTS = 'bank_accounts'
+    PAYCODES = 'paycodes'
+    EVENTS = 'events'
