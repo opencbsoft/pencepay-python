2d4e8b70c487028df6343dbc33634e7eb285ef03
Luci Furtun <lucianfurtun@gmail.com>
2017-02-20 21:33:41 +0200

    Add services

---
+++ pencepay/tests/test_transactions.py
@@ -0,0 +1,133 @@
+import os
+
+import pytest
+
+from pencepay.request import CustomerRequest, CreditCardRequest, AddressRequest, TransactionRequest
+from pencepay.request import AmountTransactionRequest, SettingsRequest
+from pencepay.services import Transaction
+from pencepay.settings.choices import CredentialsChoices
+from pencepay.settings.config import Context
+
+
+class TestTransactionService:
+    @classmethod
+    def setup_class(cls):
+        cls.customer_request = CustomerRequest(
+            firstName='John',
+            lastName='Hancock',
+            email='hancock@server.com'
+        )
+        cls.card_request = CreditCardRequest(
+            cardholderName='John Hancock',
+            number='4350100010001002',
+            cvv='313',
+            expiryMonth='12',
+            expiryYear='2018'
+        )
+        cls.address_request = AddressRequest(
+            city='Zagreb',
+            postalCode='10000',
+            countryCode='HR'
+        )
+        cls.transaction_request = TransactionRequest(
+            orderId='1234',
+            amount=55.5,
+            currencyCode='USD',
+            customer=cls.customer_request,
+            billingAddress=cls.address_request,
+            creditCard=cls.card_request
+        )
+
+    def setup_method(self, method):
+        Context.set_public_key(os.environ.get(CredentialsChoices.public_key))
+        Context.set_secret_key(os.environ.get(CredentialsChoices.secret_key))
+
+    def test_generate_checkout_parameters(self):
+        req = TransactionRequest()
+
+        req.orderId = '123456'
+        req.description = 'Holiday in Croatia, June 2015'
+        req.amount = 10.99
+        req.currencyCode = 'EUR'
+        req.cancelUrl = 'cancel-url-on-your-server'
+        req.redirectUrl = 'redirect-url-on-your-server'
+        req.billingAddress = self.address_request
+        req.customer = self.customer_request
+        req.settings = SettingsRequest(locale='hr-HR', reserveFundsOnly=True)
+
+        Context.api_version = '1.0.4'
+        Context.public_key = 'your-public-key'
+        Context.secret_key = 'your-secret-key'
+
+        data = Transaction.generate_checkout_parameters(request=req)
+        expected = {
+            'amount': 10.99,
+            'currencyCode': 'EUR',
+            'orderId': '123456',
+            'description': 'Holiday in Croatia, June 2015',
+
+            'customer.firstName': 'John',
+            'customer.lastName': 'Hancock',
+            'customer.email': 'hancock@server.com',
+            'billingAddress.city': 'Zagreb',
+            'billingAddress.postalCode': '10000',
+            'billingAddress.countryCode': 'HR',
+            'settings.reserveFundsOnly': True,
+            'settings.locale': 'hr-HR',
+            'cancelUrl': 'cancel-url-on-your-server',
+            'redirectUrl': 'redirect-url-on-your-server',
+            'apiVersion': '1.0.4',
+            'publicKey': 'your-public-key',
+            'signature': '4b13c88732ee001eae9dc30c69ea826949ac0c12afc6c51c2082163e57c277c5',
+        }
+
+        assert data == expected
+
+    @pytest.mark.skip
+    def test_create(self):
+        response = Transaction().create(request=self.transaction_request)
+
+        assert response['status_code'] == 200
+
+    @pytest.mark.skip
+    def test_create_with_card_uid(self):
+        transaction_request = TransactionRequest()
+        transaction_request.creditCardUid = 'card_X8I6pT7g7c7nxI'
+        transaction_request.amount = 22
+        transaction_request.currencyCode = 'EUR'
+
+        response = Transaction().create(request=transaction_request)
+
+        assert response['status_code'] == 200
+
+    def test_find(self):
+        response = Transaction().find(uid='txn_bfXLikzGG5igxn')
+
+        assert response['status_code'] == 200
+
+    def test_search(self):
+        response = Transaction().search(params={'currencyCode': 'EUR'})
+
+        assert response['status_code'] == 200
+
+    @pytest.mark.skip
+    def test_void(self):
+        response = Transaction().void(uid='txn_ACenirxGG5ioxA')
+
+        assert response['status_code'] == 200
+
+    @pytest.mark.skip
+    def test_capture(self):
+        req = AmountTransactionRequest(amount=55)
+
+        response = Transaction().capture(uid='txn_ACenirxGG5ioxA', request=req)
+
+        assert response['status_code'] == 200
+
+    @pytest.mark.skip
+    def test_refund(self):
+        req = AmountTransactionRequest(amount=55)
+
+        response = Transaction().refund(uid='txn_ACenirxGG5ioxA', request=req)
+
+        assert response['status_code'] == 200
--- pencepay/utils/base.py
+++ pencepay/utils/base.py
@@ -1,6 +1,9 @@
 from serpy import Field
 
+from pencepay.settings.choices import ActionChoices
+from pencepay.settings.endpoints import ENDPOINTS
 from pencepay.utils.functions import flatten_dict
+from pencepay.utils.http_client import HttpClient
 from pencepay.utils.serializer import Serializer
 
 
@@ -41,3 +44,67 @@ class BaseRequest(metaclass=RequestMetaclass):
         Implement this method to add custom validation.
         """
         pass
+
+
+class BaseService(object):
+    action = None
+    api = None
+    request = None
+    endpoints = ENDPOINTS
+
+    def _http_request(self, **kwargs):
+        client = HttpClient()
+
+        method = self.endpoints[self.api][self.action]['method']
+        path = self.endpoints[self.api][self.action]['path'].format(**kwargs)
+        params = {}
+
+        if self.action in (ActionChoices.CREATE, ActionChoices.UPDATE, ActionChoices.CAPTURE, ActionChoices.REFUND):
+            assert self.request is not None
+            params = self.request.get_flattened_data()
+
+        elif self.action == ActionChoices.SEARCH:
+            assert 'params' in kwargs
+            params = kwargs.get('params')
+
+        response = client.request(method=method, path=path, params=params)
+        return response
+
+
+class CRUDBasedServiceMixin:
+    action = None
+    request = None
+
+    def create(self, request):
+        self.action = 'create'
+        self.request = request
+        response = self._http_request()
+        return response
+
+    def find(self, uid: str):
+        self.action = 'find'
+        return self._http_request(uid=uid)
+
+    def search(self, params: dict):
+        self.action = 'search'
+        return self._http_request(params=params)
+
+    def update(self, uid: str, request):
+        self.action = 'update'
+        self.request = request
+        return self._http_request(uid=uid)
+
+    def delete(self, uid: str):
+        self.action = 'delete'
+        return self._http_request(uid=uid)
+
+
+class CustomerBasedServiceMixin:
+    def __init__(self, customer_uid):
+        self.customer_uid = customer_uid
+        super().__init__()
+
+    def _http_request(self, **kwargs):
+        kwargs['customer_uid'] = self.customer_uid
+
+        return super()._http_request(**kwargs)
